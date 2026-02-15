04a8e5a99fd165806353bb383f4c6463c2ec1dc1
Your Name <youremail@example.com>
2026-02-15 08:18:20 +0530

    feat: add analysis script to plot magnetization components (mx, my, mz)

---
+++ analysis/plot_magnetization.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load data: columns => t, mx, my, mz
+data = np.loadtxt("test.dat")
+
+t = data[:,0]
+mx = data[:,1]
+my = data[:,2]
+mz = data[:,3]
+
+plt.figure(figsize=(8,5))
+
+plt.plot(t, mx, linewidth=2, label="mx")
+plt.plot(t, my, linewidth=2, label="my")
+plt.plot(t, mz, linewidth=2, label="mz")
+
+plt.xlabel("Time")
+plt.ylabel("Magnetization")
+plt.title("LLG Magnetization Dynamics")
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
